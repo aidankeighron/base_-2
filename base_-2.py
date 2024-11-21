bfd3e490077685e57830a370e2e5afcf8f0d70dd
Aidan Keighron <78337317+aidankeighron@users.noreply.github.com>
2024-11-21 16:50:12 -0500

    updated algo

--- base_-2.py
+++ base_-2.py
@@ -1,34 +1,43 @@
 import math
 
-# def dec_to_base_n2(n):
-#     base_2 = ""
-#     print(bin(n)[2:])
-#     while n >= 1:
-#         base_2 += str(n&1)
-#         n >>= 1
-#     print(base_2[::-1])
-
 def dec_to_base_n2(n):
-    for i in range(n):
-        print(i, int(math.pow(-2, i)))
+    if n == 0:
+        return "0"
+ 
+    base = "01"
+    while n != 0:
+        rem = n % -2
+        n = int(n/-2)
+
+        if (rem < 0):
+            rem += 2
+            n += 1
+
+        base = str(rem) + base
+    return base
+
+def dec_to_base_n2_min(n):
+    if n == 0:
+        return "0"
+    base = "01"
+    while (rem := n % 2 or True) and (n := int(n/-2) + 1 if rem < 0 else 0 != 0):
+        base = str(2+rem if rem < 0 else rem) + base
+    return base
 
 def base_n2_to_dec(n2) -> int:
     out = 0
     for place, digit in enumerate(n2[::-1]):
         out += int(digit) and int(math.pow(-2, place))
-    print(out)
     return out
 
+def base_n2_to_dec_min(n2) -> int:
+    return sum(int(digit) and int(math.pow(-2, place)) for place, digit in enumerate(n2[::-1]))
 
-# 16 -8 4 -2 1
-dec_to_base_n2(8)
-base_n2_to_dec('000')
-base_n2_to_dec('001')
-base_n2_to_dec('010')
-base_n2_to_dec('011')
-base_n2_to_dec('100')
-base_n2_to_dec('101')
-base_n2_to_dec('110')
-base_n2_to_dec('111')
-# dec_to_base_n2(2)
-# dec_to_base_n2(3)
+base_n2 = dec_to_base_n2(13)
+print(base_n2, base_n2 == '11101')
+print(base_n2_to_dec('1010'), base_n2_to_dec('1010') == -10, base_n2_to_dec('1010') == base_n2_to_dec_min('1010'))
+print(base_n2_to_dec(base_n2), base_n2_to_dec(base_n2) == 13, base_n2_to_dec(base_n2) == base_n2_to_dec_min(base_n2))
+
+for i in range(13, 100, 2):
+    print(i)
+    print(base_n2_to_dec(dec_to_base_n2(i)) == i)
